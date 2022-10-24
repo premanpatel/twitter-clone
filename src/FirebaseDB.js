ba54446089e1f903ff6c9cc53736d65f02a2ef0b
Premanshu Patel <pmpatel@udel.edu>
2022-10-24 16:37:55 -0400

    fixing ui change when logged in

--- src/FirebaseDB.js
+++ src/FirebaseDB.js
@@ -90,6 +90,7 @@ export function createNewUser(auth, email, password, username) {
       // Signed in
       const user = userCredential.user;
       writeUserData(user.uid, username, email, []);
+      logInUser(email, password)
       alert("Created new user and signed in");
       // ...
     })

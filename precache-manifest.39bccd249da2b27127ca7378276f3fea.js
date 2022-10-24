75c3ed07d597361cba83b76d360f15b7c7921bc0
Premanshu Patel <pmpatel@udel.edu>
2022-10-24 15:03:06 -0400

    Updates

---
+++ precache-manifest.39bccd249da2b27127ca7378276f3fea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b79a09136da30138ebc",
+    "url": "/twitter-clone/static/css/main.45a360d8.chunk.css"
+  },
+  {
+    "revision": "8b79a09136da30138ebc",
+    "url": "/twitter-clone/static/js/main.44fa0b7e.chunk.js"
+  },
+  {
+    "revision": "510ca451f09286d33f39",
+    "url": "/twitter-clone/static/js/runtime~main.fca0ed23.js"
+  },
+  {
+    "revision": "cbbd5695f5821844e4fb",
+    "url": "/twitter-clone/static/css/2.c5ce3748.chunk.css"
+  },
+  {
+    "revision": "cbbd5695f5821844e4fb",
+    "url": "/twitter-clone/static/js/2.0afe454e.chunk.js"
+  },
+  {
+    "revision": "b390a36d279b692bc1d3",
+    "url": "/twitter-clone/static/js/3.e61989fa.chunk.js"
+  },
+  {
+    "revision": "55c24fabc9b438cde5ab1717827e9365",
+    "url": "/twitter-clone/index.html"
+  }
+];
